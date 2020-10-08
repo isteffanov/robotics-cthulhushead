cf017bcc9360e03780dbb222d59640e431488e43
Ivan Stefanov <isteffanov@gmail.com>
2020-10-09 00:44:12 +0300

    commit functional serial communication

--- serial_comm_arduino.ino
+++ serial_comm_arduino.ino
@@ -1,15 +1,32 @@
 
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
-
+  Serial.flush();
 }
 
 void loop() {
-  int read_first = Serial.read();
-  Serial.print(read_first);
-  Serial.print(" ");
-  int read_second = Serial.read();
-  Serial.println(read_second);
+  if(Serial.available() && Serial.read() == '\0'){
+    
+    int data_first = Serial.read();
+    int data_second = Serial.read();
+    int data_third = Serial.read();
+    int data_forth = Serial.read();
+    int data_fifth = Serial.read();
+    int data_sixth = Serial.read();
+    
+    Serial.print(data_first);
+    Serial.print(" ");
+    Serial.print(data_second);
+    Serial.print(" ");
+    Serial.print(data_third);
+    Serial.print(" ");
+    Serial.print(data_forth);
+    Serial.print(" ");
+    Serial.print(data_fifth);
+    Serial.print(" ");
+    Serial.println(data_sixth);
+
+    delay(100);
+  }
 }
